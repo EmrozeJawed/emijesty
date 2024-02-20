d5d471d0b68723561519f87699731fea19ec60ef
EmrozeJawed <eemilyllc@gmail.com>
2024-02-20 18:54:42 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import NewProducts from "./components/NewProducts";
+import Testimonial from "./components/Testimonial";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <NewProducts />
+      <Testimonial />
+    </main>
+  );
+}
